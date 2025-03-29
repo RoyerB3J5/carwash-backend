ff11d8189943977e7c5e19fe1cb304240961354a
Royer <royer.boni13@gmail.com>
2025-03-29 17:19:35 -0500

    Fixed controllers

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -1,5 +1,89 @@
-async function deleteExpense(req, res) {}
+import Expenses from "../models/expenses.model.js";
+async function deleteExpense(req, res) {
+  try {
+    const idMyUser = req.user.uid;
+    const { id } = req.params;
+    const deleteExpense = await Expenses.findOneAndDelete({
+      idMyUser,
+      _id: id,
+    });
+    if (!deleteExpense) {
+      return res.status(404).json({
+        error: "Expense not found",
+      });
+    }
+    return res.status(200).json({
+      message: "Expense deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+}
 
-async function getExpensesByMonth(req, res) {}
-async function postNewExpense(req, res) {}
+async function getExpensesByMonth(req, res) {
+  try {
+    const { month, year } = req.params;
+    const idMyUser = req.user.uid;
+
+    const numericMonth = parseInt(month) - 1;
+    const numericYear = parseInt(year);
+
+    if (
+      isNaN(numericMonth) ||
+      numericMonth < 0 ||
+      numericMonth > 11 ||
+      isNaN(numericYear)
+    ) {
+      return res.status(400).json({
+        error: "Invalid month or year",
+      });
+    }
+    const peruOffset = -5 * 60;
+    const startDate = new Date(Date.UTC(numericYear, numericMonth, 1, 5, 0, 0));
+    const endDate = new Date(
+      Date.UTC(
+        numericMonth === 11 ? numericYear + 1 : numericYear,
+        numericMonth === 11 ? 0 : numericMonth + 1,
+        1,
+        5,
+        0,
+        0,
+      ),
+    );
+    const expenses = await Expenses.find({
+      idMyUser: idMyUser,
+      createdAt: {
+        $gte: startDate,
+        $lt: endDate,
+      },
+    }).sort({ createdAt: 1 });
+    return res.status(200).json(expenses);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+}
+async function postNewExpense(req, res) {
+  const idMyUser = req.user.uid;
+  const newExpense = req.body;
+  const newExpenses = {
+    ...newExpense,
+    idMyUser: idMyUser,
+  };
+
+  try {
+    const expense = await Expenses.create(newExpenses);
+    return res.status(201).json(expense);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+}
 export { deleteExpense, getExpensesByMonth, postNewExpense };
--- controllers/typeExpenses.controller.js
+++ controllers/typeExpenses.controller.js
@@ -1,5 +1,64 @@
-async function getTypeExpenses(req, res) {}
-async function deleteTypeItem(req, res) {}
-async function upsertTypes(req, res) {}
+import {TypeExpenses} from "../models/TypeExpenses.js";
+
+async function getTypeExpenses(req, res) {
+  try {
+    const idMyUser = req.user.uid;
+    const expenses = await TypeExpenses.find({ idMyUser });
+    return res.status(200).json(expenses);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+}
+async function deleteTypeItem(req, res) {
+  const { id } = req.params;
+  const { idMyUser } = req.user.uid;
+  try {
+    const deleteTypeExpense = await TypeExpenses.findOneAndDelete({
+      id,
+      idMyUser,
+    });
+    if (!deleteTypeExpense) {
+      return res.status(404).json({ message: "Tipo de gasto no encontrado" });
+    }
+    return res.status(200).json({ message: "Tipo de gasto eliminado" });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Error al eliminar el tipo de gasto" });
+  }
+}
+async function upsertTypes(req, res) {
+  try {
+const  idMyUser  = req.user.uid
+  const dataUpsert = Array.isArray(req.body) ? req.body : JSON.parse(req.body)
+
+  if (!dataUpsert.every(item => typeof item === 'object' && ('_id' in item || name in item))) {
+  return res.status(400).json({error: "Formato de datos invalidos"})
+}
+  const bulkOperations = dataUpsert.map(item => {
+    const {_id, ...rest} =item
+    if (_id){
+      return {
+        updatedOne: {
+          filter: {_id, idMyUser},
+          update: {$set : { ...rest, idMyUser }},
+          upsert: false
+        }
+        },
+    },
+    return {
+      insertOne: {
+        document: {...rest, idMyUser}
+      }
+    }
+      }), 
+    const result = await TypeExpenses.bulkWrite(bulkOperations)
+    return res.status(200).json(result)
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+}
 
 export { getTypeExpenses, deleteTypeItem, upsertTypes };
